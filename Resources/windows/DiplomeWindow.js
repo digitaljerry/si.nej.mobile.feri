15847ff235b7a339cb954a0fea9cbf5c03777914
Jernej Zorec <jernejz@gmail.com>
2012-02-02 17:29:27 +0100

    Showing diplome from local stored html

--- Resources/windows/DiplomeWindow.js
+++ Resources/windows/DiplomeWindow.js
@@ -11,78 +11,22 @@
             fullscreen: false
         });
         
-        var data = [];
+        var content = '';
         
-        data.push(feri.ui.createHeaderRow('Aktualni zagovori'));
+        var f1 = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, 'pages/zadnjizagovori.html');
+        content = content + f1.read();
+        var f2 = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, 'pages/zadnjediplome.html');
+        content = content + f2.read();
         
-        //var sessions = Drupal.entity.db('main', 'diplome').loadMultiple(uids, ['uid'], false);
-        //for (var sessionNum = 0, numSessions = sessions.length; sessionNum < numSessions; sessionNum++) {
-            /*var sessionTitle = 'NIZKOCENOVNI OPTIČNI SPEKTRALNI ANALIZATOR';
-            var sessionCandidate = 'LILEK MATIC';
-            var sessionRoom = 'senatni sobi G2 (III. nadstropje)';
-            var sessionDate = '13.01.2012 ob 08:00';
-            
-            var sessionRow = Ti.UI.createTableViewRow({
-                hasChild: true,
-                className: 'cs_session',
-                selectedColor: '#000',
-                backgroundColor: '#fff',
-                color: '#000',
-                uid: '1',
-                sessionTitle: sessionTitle,
-                height: 'auto',
-                layout: 'vertical',
-                focusable: true
-            });
-			
-            var leftSpace = 10;
-            var titleColor = '#1C4980';
-            
-            var titleLabel = Ti.UI.createLabel({
-                text: sessionTitle,
-                font: {
-                    fontSize: 16,
-                    fontWeight: 'bold'
-                },
-                color: titleColor,
-                left: leftSpace,
-                top: 10,
-                bottom: 10,
-                right: 10,
-                height: 'auto',
-                touchEnabled: false
-            });
-
-            // Some sessions have multiple people
-            var candidateLabel = Ti.UI.createLabel({
-                text: sessionCandidate,
-                font: {
-                    fontSize: 14,
-                    fontWeight: 'normal'
-                },
-                color: '#000',
-                left: leftSpace,
-                top: 4,
-                bottom: 10,
-                right: 10,
-                height: 'auto',
-                touchEnabled: false
-            });
-
-            sessionRow.add(titleLabel);
-            sessionRow.add(candidateLabel);
-			
-			data.push(sessionRow);*/
-        //}
-        
-        data.push(feri.ui.createHeaderRow('Zadnje diplome'));
-        
-        feri.diplomeTableView = Titanium.UI.createTableView({
-            data: data
+        var webview = Ti.UI.createWebView({
+        	html:'<html><head></head><body>'+content+'</body></html>',
+        	evaljs:true
         });
+        //webview.evalJS("showFeeds('http://dkum.uni-mb.si/rss.php?o=3&v=dip','oglasna_deska')");
+        //webview.scalesPageToFit = true;
         
-        diplomeWindow.add(feri.diplomeTableView);
-
+        diplomeWindow.add(webview);
+        
         return diplomeWindow;
     };
 })();
