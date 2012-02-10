c45d885bce93b45de4a35e7cb04add970919ea29
Jernej Zorec <jernejz@gmail.com>
2012-02-11 00:48:46 +0100

    Injecting CSS into webview

--- Resources/windows/InformacijeWindow.js
+++ Resources/windows/InformacijeWindow.js
@@ -13,21 +13,27 @@
             fullscreen: false
         });
         
-        //infoWindow.add(feri.ui.createTabbedScrollableView({data:data}));
         var webview = Ti.UI.createWebView({
             url: feri.informacije_url,
             width: '100%',
-            height: '100%'
+            height: '100%',
+            visible: false
         });
         
-        //injecting css for better display on mobile
-        //var cssContent = Titanium.Filesystem.getFile('pages/styles_info_injection.css');
-        // make it available as a variable in the webview    
-		//webview.evalJS("var myCssContent = " + JSON.stringify(String(cssContent.read())) + ";");
-		// create the style element with the css content    
-		//webview.evalJS("var s = document.createElement('style'); s.setAttribute('type', 'text/css'); s.innerHTML = myCssContent; document.getElementsByTagName('head')[0].appendChild(s);");
+        // web controls
+		var bb2 = Titanium.UI.createButtonBar({
+			labels:['Back', 'Reload', 'Forward'],
+			backgroundColor:'#003'
+		});
 		
-		// inject our css when the web view finishes loading (because we need to inject into the head element)
+		var flexSpace = Titanium.UI.createButton({
+			systemButton:Titanium.UI.iPhone.SystemButton.FLEXIBLE_SPACE
+		});
+		
+		infoWindow.setToolbar([flexSpace,bb2,flexSpace]);
+        
+        //injecting css for better display on mobile
+        // inject our css when the web view finishes loading (because we need to inject into the head element)
 		webview.addEventListener('load', function () {
 		    // first, specify the CSS file that we should load
 		    var cssFileName = 'pages/styles_info_injection.css';
@@ -38,29 +44,48 @@
 		        .split('\r').join('')
 		        .split('\n').join(' ')
 		        .split('"').join('\\"');
+		    
 		    // and run the JavaScript to inject the CSS by setting the URL of the web view
 		    // (hint: try running "javascript:alert('Hello, world!');" in your own browser to see what happens
-		    webview.url =
-		        'javascript:(function evilGenius(){' 
-		            + 'var s=document.createElement("style");'
-		            + 's.setAttribute("type","text/css");'
-		            + 's.innerHTML="' + contents + '";'
-		            + 'document.getElementsByTagName("head")[0].appendChild(s);'
-		        + '})();';
+		    webview.evalJS('javascript:(function evilGenius(){' 
+		    				+ 'var s=document.createElement("style");'
+            				+ 's.setAttribute("type","text/css");'
+            				+ 's.innerHTML="' + contents + '";'
+            				+ 'document.getElementsByTagName("head")[0].appendChild(s);'
+        					+ '})();');
 		    // note that we don't use web.evalJS() -- it relies on Ti being injected in the DOM already but we're
 		    // running this on an external URL, so it won't have Ti injected! web.url is the same method evalJS uses
+		    
+		    webview.setVisible(true);
+		});
+		
+		webview.addEventListener('beforeload', function () {
+			webview.setVisible(false);
+		});
+		
+		webview.addEventListener('load',function(e)
+		{
+			Ti.API.debug("url = "+webview.url);
+			Ti.API.debug("event url = "+e.url);
+		});
+		bb2.addEventListener('click',function(ce)
+		{
+			if (ce.index == 0)
+			{
+				webview.goBack();
+			}
+			else if (ce.index == 1)
+			{
+				webview.reload();
+			}
+			else
+			{
+				webview.goForward();
+			}
 		});
 		
 		infoWindow.add(webview);
 		
-		// adding the view
-		/*infoWindow.add(webview);
-        infoWindow.addEventListener('focus', function (e) {
-            webview.url = feri.informacije_url;
-            webview.height = '100%';
-            webview.width = '100%';
-        });*/
-        
-        return infoWindow;
+		return infoWindow;
     };
 })();
