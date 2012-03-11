dace5514f3684f9d6028fad335fe68dfc6fffca3
Jernej Zorec <jernejz@gmail.com>
2012-03-11 23:26:27 +0100

    Removed start mode option

--- Resources/windows/NastavitveWindow.js
+++ Resources/windows/NastavitveWindow.js
@@ -13,14 +13,7 @@
         // Create the table view
         var inputData = [
 			{title:'Dashboard', switchAppUse: 'dashboard', header:'Oblika'},
-			{title:'Zavihki', switchAppUse: 'tabs'},
-			{title:'Dashboard', hasCheck:true, header:'Začetna stran'},
-			{title:'Deska'},
-			{title:'Urnik'},
-			{title:'Zaposleni'},
-			{title:'Zemljevid'},
-			{title:'Diplome'},
-			{title:'Informacije'}
+			{title:'Zavihki', switchAppUse: 'tabs'}
 		];
 		
 		if ( feri.useDashboard == true )
