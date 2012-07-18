ce6c9eaaeb8758c4c5fe340fa8285f11b8cb71f7
Jernej Zorec <jernejz@gmail.com>
2012-07-18 13:05:13 +0200

    Turn off alert because not testing this at the moment
    Signed-off-by: Jernej Zorec <jernejz@gmail.com>

--- Resources/app/push.js
+++ Resources/app/push.js
@@ -53,10 +53,10 @@
 		}, function (e) {
 			if (e.success) {
 				var user = e.users[0];
-				alert(	'Success:\\n' +
+				/*alert(	'Success:\\n' +
 						'id: ' + user.id + '\\n' +
 						'first name: ' + user.first_name + '\\n' +
-						'last name: ' + user.last_name);
+						'last name: ' + user.last_name);*/
 			} else {
 				alert(	'Error:\\n' +
 						((e.error && e.message) || JSON.stringify(e)));
